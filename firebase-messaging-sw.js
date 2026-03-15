24423eced70bd350027948af70441e43e5b1990a
samruddhipathare25-creator <samruddhipathare25@gmail.com>
2026-03-16 01:45:22 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,1 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
-firebase.initializeApp({
-  apiKey: "AIzaSyC1HbuxatlVcEBjafFU5NWcXTOYoF0KA9U",
-  authDomain: "sehataiguide.firebaseapp.com",
-  projectId: "sehataiguide",
-  messagingSenderId: "56986654799",
-  appId: "1:56986654799:web:eba164f34d30b31635323a"
-});
-
-const messaging = firebase.messaging();
