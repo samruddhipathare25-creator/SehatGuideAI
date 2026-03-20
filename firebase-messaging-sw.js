85ddd1b585267a2ec4622cdeb13ee5065f63e298
samruddhipathare25-creator <samruddhipathare25@gmail.com>
2026-03-20 12:35:04 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,23 @@
 
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC1HbuxatlVcEBjafFU5NWcXTOYoF0KA9U",
+  authDomain: "sehataiguide.firebaseapp.com",
+  projectId: "sehataiguide",
+  messagingSenderId: "56986654799",
+  appId: "1:56986654799:web:eba164f34d30b31635323a"
+});
+
+const messaging = firebase.messaging();
+
+// Background messages (when app is closed)
+messaging.onBackgroundMessage(function(payload) {
+  console.log("Background message:", payload);
+
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/images/icon.png"
+  });
+});
